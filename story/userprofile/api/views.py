484e42cdf118f466f153523af7747a0b5ab16c4c
ngenzir <ngenzir@gmail.com>
2021-02-16 22:02:49 +0200

    views comments

--- story/userprofile/api/views.py
+++ story/userprofile/api/views.py
@@ -16,12 +16,7 @@ from ..serializers import PublicProfileSerializer
 User = get_user_model()
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
-# @api_view(['GET'])
-# @permission_classes([IsAuthenticated])
-# def user_profile_detail_view(request, username, *args, **kwargs):
-#     current_user = request.user
-#     to_follow_user = ??
-#     return Response({}, status=200)
+
 
 @api_view(['GET', 'POST'])
 def profile_detail_api_view(request, username, *args, **kwargs):
@@ -45,25 +40,3 @@ def profile_detail_api_view(request, username, *args, **kwargs):
     return Response(serializer.data, status=200)
 
 
-# @api_view(['GET', 'POST'])
-# @permission_classes([IsAuthenticated])
-# def user_follow_view(request, username, *args, **kwargs):
-#     me = request.user
-#     other_user_qs = User.objects.filter(username=username)
-#     if me.username == username:
-#         my_followers = me.profile.followers.all()
-#         return Response({"count": my_followers.count()}, status=200)
-#     if not other_user_qs.exists():
-#         return Response({}, status=404)
-#     other = other_user_qs.first()
-#     profile = other.profile
-#     data = request.data or {}
-#     action = data.get("action")
-#     if action == "follow":
-#         profile.followers.add(me)
-#     elif action == "unfollow":
-#         profile.followers.remove(me)
-#     else:
-#         pass
-#     data = PublicProfileSerializer(instance=profile, context={"request": request})
-#     return Response(data.data, status=200)
