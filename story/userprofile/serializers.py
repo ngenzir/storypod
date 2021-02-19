92e649273fa67394d15a337b2013bc3558781c0a
ngenzir <ngenzir@gmail.com>
2021-02-19 09:56:06 +0200

    r

--- story/userprofile/serializers.py
+++ story/userprofile/serializers.py
@@ -24,7 +24,7 @@ class PublicProfileSerializer(serializers.ModelSerializer):
         ]
     
     def get_is_following(self, obj):
-        # request???
+        # request
         is_following = False
         context = self.context
         request = context.get("request")
