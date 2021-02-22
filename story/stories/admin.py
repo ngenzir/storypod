13e014f536a441dfdf50832411e18fa5db6c54ea
ngenzir <ngenzir@gmail.com>
2021-02-22 23:16:03 +0200

    stories admin

--- story/stories/admin.py
+++ story/stories/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Story, StoryLike
+
+
+class StoryLikeAdmin(admin.TabularInline):
+    model = StoryLike
+
+class StoryAdmin(admin.ModelAdmin):
+    inlines = [StoryLikeAdmin]
+    list_display = ['__str__', 'user']
+    search_fields = ['content', 'user__username', 'user__email']
+    class Meta:
+        model = Story
+
+admin.site.register(Story, StoryAdmin)
+
+
