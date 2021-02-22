de056b825964b2dc8b184ec08ff3ecbcf80d224b
ngenzir <ngenzir@gmail.com>
2021-02-22 22:58:51 +0200

    stories models

--- story/stories/models.py
+++ story/stories/models.py
@@ -1,3 +1,63 @@
+import random
+from django.conf import settings
 from django.db import models
+from django.db.models import Q
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class StoryLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    story = models.ForeignKey("Story", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class StoryQuerySet(models.QuerySet):
+    def by_username(self, username):
+        return self.filter(user__username__iexact=username)
+
+    def feed(self, user):
+        profiles_exist = user.following.exists()
+        followed_users_id = []
+        if profiles_exist:
+            followed_users_id = user.following.values_list("user__id", flat=True) # [x.user.id for x in profiles]
+        return self.filter(
+            Q(user__id__in=followed_users_id) |
+            Q(user=user)
+        ).distinct().order_by("-timestamp")
+
+class StoryManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return StoryQuerySet(self.model, using=self._db)
+
+    def feed(self, user):
+        return self.get_queryset().feed(user)
+
+class Story(models.Model):
+    # Maps to SQL data
+    # id = models.AutoField(primary_key=True)
+    parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="stories") # many users can post many stories
+    likes = models.ManyToManyField(User, related_name='story_user', blank=True, through=StoryLike)
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = StoryManager()
+    # def __str__(self):
+    #     return self.content
+    
+    class Meta:
+        ordering = ['-id']
+    
+    @property
+    def is_restory(self):
+        return self.parent != None
+    
+    def serialize(self):
+        '''
+        Feel free to delete!
+        '''
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 200)
+        }
